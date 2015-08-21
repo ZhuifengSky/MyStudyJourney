c07956cb0dedcb5d0a52090b9e572070eae1b62a
ZhuifengSky <843820873@qq.com>
2015-08-21 09:52:18 +0800

    ssm整合demo

---
+++ src/com/main/user/controller/UserController.java
@@ -0,0 +1,45 @@
+package com.main.user.controller;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.main.user.bean.UserBean;
+import com.main.user.dao.IUserDao;
+import com.main.user.model.User;
+
+/**
+ * 
+ * <p>Title:   HelloWorldController</p>
+ * <p>Description: HelloWorld控制类  </p>
+ * <p>Company:   www.edu24ol.com</p>
+ * @author   pc-zw
+ * @date     2015年8月6日下午2:49:06
+ * @version  1.0
+ */
+@Controller
+public class UserController {
+	
+	@Resource
+	private IUserDao userDaoImpl;
+	
+	@RequestMapping("/searchUser.do")
+	public String searchUser(UserBean queryBean,ModelMap map){
+		List<User> users= userDaoImpl.searchUser(queryBean);
+		map.put("users", users);
+		map.put("pageBean", queryBean);
+		map.put("queryBean", queryBean);
+		return "jsp/listUser";
+	}
+
+	
+
+
+
+	
+
+}
---
+++ src/com/main/user/service/IUserService.java
@@ -0,0 +1,25 @@
+package com.main.user.service;
+
+import java.util.List;
+
+import com.main.user.bean.UserBean;
+import com.main.user.model.User;
+
+/**
+ * 
+ * <p>Title:   IUserService.java</p>
+ * <p>Description:  用户Service接口 </p>
+ * <p>Company:   www.edu24ol.com</p>
+ * @author   pc-zw
+ * @date     2015年8月20日下午5:43:41
+ * @version  1.0
+ */
+public interface IUserService {
+
+	/**
+	 * 用户列表查询
+	 * @param queryBean
+	 * @return
+	 */
+	public List<User> searchUser(UserBean queryBean);
+}
